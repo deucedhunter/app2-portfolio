ce958a19bc4b5e3a14aab9a53d12534661f2ad04
deucedhunter <deucedhunter@gmail.com>
2024-12-12 18:11:10 +0100

    Day 23: Send emails using the form

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,3 +1,18 @@
 import streamlit as st
+from send_email import send_email
+
 
 st.header("Contact Page")
+with st.form(key='form'):
+    user_email = st.text_input("Email Address")
+    raw_message = st.text_area("Your message")
+    message = f"""\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+"""
+    button = st.form_submit_button("Submit")
+    if button:
+        send_email(message=message)
+        st.info("Email sent")
